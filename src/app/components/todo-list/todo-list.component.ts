8f2d715cf77294a685a2fa39fbf5e12c80be9fe8
Victor Muniz <victorrmunizz@gmail.com>
2023-11-10 10:52:29 -0300

    implement done function

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -3,19 +3,37 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { TodoItem } from '../../models/TodoItem';
 import { FormBuilder, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatFormFieldModule, 
+    FormsModule, 
+    ReactiveFormsModule,
+    MatIconModule
+    ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
 
-  todoList: TodoItem[] = [];
+  todoList: TodoItem[] = [
+    {
+      id: 1,
+      description: 'teste'
+    },
+    {
+      id: 2,
+      description: 'teste 2'
+    }
+  ];
   todoForm!: FormGroup;
 
   constructor(private fb: FormBuilder) {
@@ -41,5 +59,9 @@ export class TodoListComponent {
     } else {
       this.todoForm.markAllAsTouched();
     }
+  }
+
+  done(item: TodoItem) {
+    this.todoList = this.todoList.filter(todo => todo != item);
   }
 }
