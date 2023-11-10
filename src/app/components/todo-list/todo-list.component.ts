e9a9b647e080171ee456d32fc5c2e45f070b9a92
Victor Muniz <victorrmunizz@gmail.com>
2023-11-10 10:12:51 -0300

    implements style and validations to the form

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -4,25 +4,42 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { TodoItem } from '../../models/TodoItem';
+import { FormBuilder, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatInputModule, MatFormFieldModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
 
   todoList: TodoItem[] = [];
+  todoForm!: FormGroup;
 
-  addTodo(todo: HTMLInputElement) {
-    const newTodo: TodoItem = {
-      id: this.todoList.length + 1,
-      description: todo.value
-    };
+  constructor(private fb: FormBuilder) {
+    this.todoForm = this.fb.group({
+      todo: ['', Validators.required]
+    });
+  }
 
-    this.todoList.push(newTodo);
-    todo.value = '';
+  addTodo(formDirective: FormGroupDirective) {
+    if (this.todoForm.valid) {
+      const newTodo: TodoItem = {
+        id: this.todoList.length + 1,
+        description: this.todoForm.get('todo')?.value
+      };
+  
+      this.todoList.push(newTodo)
+      this.todoForm.setValue({
+        todo: ''
+      });
+
+      formDirective.resetForm();
+      this.todoForm.reset();
+    } else {
+      this.todoForm.markAllAsTouched();
+    }
   }
 }
